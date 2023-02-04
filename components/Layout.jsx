ca5b1532e88466d4765782e74e3e373cecb799ae
Subin Kim <subinlk@uci.edu>
2023-02-03 22:05:32 -0800

    base layout in process

---
+++ components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Layout = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Layout;
